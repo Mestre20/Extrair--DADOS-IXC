55a9ba3042008ccdb3d486758c9548ced7894064
Mixtel <rrsilva1996@gmail.com>
2024-06-08 11:20:39 -0300

    Adicionar arquivos de projeto.

---
+++ PROJETO 2/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace PROJETO_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(23, 69);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(208, 70);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Selecionar arquivo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(237, 69);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(484, 290);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(508, 365);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(213, 64);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "SALVAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PROJETO_2.Properties.Resources.background_de_dados_big_data_estrutura_de_rede_background_generative_inteligencia_artificial_886951_830__1_;
+            this.ClientSize = new System.Drawing.Size(814, 512);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "EXTRATOR DE DADOS";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ PROJETO 2/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using HtmlAgilityPack;
+using OfficeOpenXml;
+
+namespace PROJETO_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            // Defina o contexto da licença no construtor ou em outro ponto inicial do aplicativo
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Inicialização ao carregar o formulário, se necessário
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Arquivo de Texto (*.txt)|*.txt";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                string htmlContent = File.ReadAllText(filePath);
+
+                List<ClientInfo> clients = ExtractClientInfo(htmlContent);
+                listBox1.Items.Clear();
+                foreach (var client in clients)
+                {
+                    listBox1.Items.Add($"{client.Name} - {client.Phone} - {client.Technician}");
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Arquivo Excel (*.xlsx)|*.xlsx";
+            saveFileDialog.FileName = "clientes.xlsx";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = saveFileDialog.FileName;
+                List<ClientInfo> clients = listBox1.Items.Cast<string>()
+                    .Select(item =>
+                    {
+                        var parts = item.Split(new[] { " - " }, StringSplitOptions.None);
+                        return new ClientInfo
+                        {
+                            Name = parts[0],
+                            Phone = parts[1],
+                            Technician = parts.Length > 2 ? parts[2] : "Sem Técnico ainda"
+                        };
+                    })
+                    .ToList();
+
+                SaveToExcel(clients, filePath);
+                MessageBox.Show("Lista salva com sucesso!");
+            }
+        }
+
+        private List<ClientInfo> ExtractClientInfo(string htmlContent)
+        {
+            var doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(htmlContent);
+
+            var clients = new List<ClientInfo>();
+
+            var clientNodes = doc.DocumentNode.SelectNodes("//td[@abbr='cliente.razao']/div[contains(@style, 'text-align: left; width: 198px;')]");
+            var phoneNodes = doc.DocumentNode.SelectNodes("//td[@abbr='cliente.telefone_celular']/div[contains(@style, 'text-align: left; width: 80px;')]");
+            var technicianNodes = doc.DocumentNode.SelectNodes("//td[@abbr='funcionarios.funcionario']//div[@style='text-align: left; width: 76px;']");
+
+            if (clientNodes != null && phoneNodes != null && technicianNodes != null)
+            {
+                int count = Math.Min(clientNodes.Count, Math.Min(phoneNodes.Count, technicianNodes.Count));
+
+                for (int i = 0; i < count; i++)
+                {
+                    string clientName = clientNodes[i].InnerText.Trim();
+                    string phone = phoneNodes[i].InnerText.Trim();
+                    string technician = "Sem Técnico ainda";
+
+                    if (technicianNodes.Count > i)
+                    {
+                        var technicianNode = technicianNodes[i];
+                        if (!technicianNode.InnerText.Trim().Equals("&nbsp;", StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            technician = technicianNode.Attributes["title"]?.Value.Trim() ?? "Sem Técnico ainda";
+                        }
+                    }
+
+                    clients.Add(new ClientInfo { Name = clientName, Phone = phone, Technician = technician });
+                }
+            }
+
+            return clients;
+        }
+
+        private void SaveToExcel(List<ClientInfo> clients, string filePath)
+        {
+            // Defina o contexto da licença
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+            using (var package = new ExcelPackage())
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Clientes");
+                worksheet.Cells[1, 1].Value = "Nome";
+                worksheet.Cells[1, 2].Value = "Telefone";
+                worksheet.Cells[1, 3].Value = "Técnico";
+
+                for (int i = 0; i < clients.Count; i++)
+                {
+                    worksheet.Cells[i + 2, 1].Value = clients[i].Name;
+                    worksheet.Cells[i + 2, 2].Value = clients[i].Phone;
+                    worksheet.Cells[i + 2, 3].Value = clients[i].Technician;
+                }
+
+                package.SaveAs(new FileInfo(filePath));
+            }
+        }
+    }
+
+    public class ClientInfo
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Technician { get; set; }
+    }
+}
